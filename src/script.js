052f80551ab90a0a5cc86e7da27d8ad0b65078c0
NiPpOoOz <82100496+NiPpOoO@users.noreply.github.com>
2025-12-11 15:36:54 +0300

    Add markerhandler component for A-Frame

--- src/script.js
+++ src/script.js
@@ -1,1 +1,11 @@
+AFRAME.registerComponent('markerhandler', {
+  init: function () {
+    this.el.addEventListener('markerFound', () => {
+      console.log("Маркер найден!");
+    });
+    this.el.addEventListener('markerLost', () => {
+      console.log("Маркер потерян!");
+    });
+  }
+});
 
